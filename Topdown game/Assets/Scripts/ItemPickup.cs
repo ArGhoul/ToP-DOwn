f33770f289ccd76692264c4ec55e6845af5da877
ArGhoul <59735113+ArGhoul@users.noreply.github.com>
2020-01-10 16:57:53 +0100

    Itempickup changes
    volgende x moet ik de visibility instantiaten van de gun in de hand, thijs maakt als het goed is de character met bones ofs

---
+++ Topdown game/Assets/Scripts/ItemPickup.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+
+public class ItemPickup : Interactable
+{   
+
+    public new GameObject name;    
+
+    public override void Interact()
+    {
+        base.Interact();
+
+        PickUp();
+    }
+
+    void PickUp()
+    {
+        Debug.Log("picking up item");
+
+        Destroy(name);
+        
+    }
+}
