6135dea5c957051564a949ab81f410c8ed83ae4f
ArGhoul <59735113+ArGhoul@users.noreply.github.com>
2020-01-22 19:51:12 +0100

    AiBeginning

---
+++ Topdown game/Assets/Scripts/Controllers/AIEnemyControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIEnemyControl : MonoBehaviour
+{
+
+    public float lookRadius = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+}
