a1fe2956910de4da0366617e222f66911ab6bba0
ArGhoul <59735113+ArGhoul@users.noreply.github.com>
2020-01-23 16:46:14 +0100

    ahfba

--- Topdown game/Assets/Scripts/Controllers/AIEnemyControl.cs
+++ Topdown game/Assets/Scripts/Controllers/AIEnemyControl.cs
@@ -1,16 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class AIEnemyControl : MonoBehaviour
 {
 
     public float lookRadius = 10f;
 
+    Transform target;
+    NavMeshAgent agent;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        target = PlayerManager.instance.player.transform;
+        agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
